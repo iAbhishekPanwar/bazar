f2af6a6d7e903a2fff56fd91e85b2e9d547b4c47
Abhishek Panwar <imabhishek612@gmail.com>
2023-06-15 00:50:05 +0530

    cart functional, saving data on redux persist

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import CartItem from "../components/CartItem";
 
 const Cart = () => {
-  // const productData = useSelector((state) => state.bazar.productData);
-  // console.log(productData);
+  const productData = useSelector((state) => state.bazar.productData);
+  const [totalAmt, setTotalAmt] = useState("");
+  useEffect(() => {
+    let price = 0;
+    productData.map((item) => {
+      price += item.price * item.quantity;
+      return price;
+    });
+    setTotalAmt(price.toFixed(2));
+  }, [productData]);
   return (
     <div>
       <img
@@ -19,7 +27,9 @@ const Cart = () => {
             <h2 className="text-2xl font-medium">cart total</h2>
             <p className="flex items-center gap-4 text-base">
               Subtotal{" "}
-              <span className="font-titleFont font-bold text-lg">${200}</span>
+              <span className="font-titleFont font-bold text-lg">
+                ${totalAmt}
+              </span>
             </p>
             <p className="flex items-start gap-4 text-base">
               Shipping{" "}
@@ -30,7 +40,7 @@ const Cart = () => {
             </p>
           </div>
           <p className="font-titleFont font-semibold flex justify-between mt-6">
-            Total <span className="text-xl font-bold">${200}</span>
+            Total <span className="text-xl font-bold">${totalAmt}</span>
           </p>
           <button
             onClick={() => {}}
