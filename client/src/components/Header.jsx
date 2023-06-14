a692764eb393d0a015cd7658b718bf18c6c54c8c
Abhishek Panwar <imabhishek612@gmail.com>
2023-06-14 07:53:27 +0530

    cart page structure done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { cartImg, logoDark } from "../assets/index";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const productData = useSelector((state) => state.bazar.productData);
+  console.log(productData);
   return (
     <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-800 font-titleFont sticky top-0 z-50">
       <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
@@ -29,12 +32,14 @@ const Header = () => {
               Blog
             </li>
           </ul>
-          <div className="relative">
-            <img className="w-6" src={cartImg} alt="cartImage" />
-            <span className="absolute w-6 top-2 left-0 text-sm flex items-center justify-center font-semibold">
-              0
-            </span>
-          </div>
+          <Link to="/cart">
+            <div className="relative">
+              <img className="w-6" src={cartImg} alt="cartImage" />
+              <span className="absolute w-6 top-2 left-0 text-sm flex items-center justify-center font-semibold">
+                {productData.length}
+              </span>
+            </div>
+          </Link>
           <img
             className="w-8 h-8 rounded-full"
             src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
